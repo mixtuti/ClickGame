3c2a567bdb976b4737c767df2cbe5ea1c4bcafd2
mixtuti桜餅 <88471532+mixtuti@users.noreply.github.com>
2023-02-20 14:51:38 +0900

    Update connect.php

--- click/api/connect.php
+++ click/api/connect.php
@@ -1,15 +1,9 @@
 <?php
 /**
  * DB接続
- * 
- * Copyright (c) 2018 Kazuma Kasagi
- * Released under the MIT license
- * http://opensource.org/licenses/mit-license.php
  */
 
-//TODO:以下のユーザ名、パスワード、データベース名を自身のデータベースのものに書き換えてください。
-//     ユーザ名、パスワードはAdminerで入力している自身の学籍番号です。
-//               接続先       ユーザ名    パスワード  データベース名
+//                  接続先       ユーザ名    パスワード       データベース名
 $db = new mysqli('localhost', 'HW20A086', 'HW20A086', 'HW20A086_click');
 if($db->connect_errno){
 	echo "DB接続失敗";
