365df5c350cc28c1a879384c4ac887b600016738
mixtuti桜餅 <88471532+mixtuti@users.noreply.github.com>
2023-02-20 14:54:42 +0900

    Update prototype.js

--- click/prototype.js
+++ click/prototype.js
@@ -1,11 +1,3 @@
-/*  Prototype JavaScript framework, version 1.7.1
- *  (c) 2005-2010 Sam Stephenson
- *
- *  Prototype is freely distributable under the terms of an MIT-style license.
- *  For details, see the Prototype web site: http://www.prototypejs.org/
- *
- *--------------------------------------------------------------------------*/
-
 var Prototype = {
 
   Version: '1.7.1',
