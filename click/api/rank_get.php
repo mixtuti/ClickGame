a87e31c732cabf7750eb6ec712e85a22d811785b
mixtuti桜餅 <88471532+mixtuti@users.noreply.github.com>
2023-02-20 14:53:32 +0900

    Update rank_get.php

--- click/api/rank_get.php
+++ click/api/rank_get.php
@@ -1,10 +1,6 @@
 <?php
 /**
  * ランキング取得API
- * 
- * Copyright (c) 2018 Kazuma Kasagi
- * Released under the MIT license
- * http://opensource.org/licenses/mit-license.php
  */
 
 //$resultは戻り値 error=1なら失敗、0なら成功
@@ -22,10 +18,7 @@ $result = array(
 
 include "connect.php";
 
-//TODO:以下の$sql変数に、SELECT文のSQLを記述せよ。
-//     テーブルはranking。取得したいカラムはname, score。
-//     SELECT文の末尾に "ORDER BY score DESC LIMIT 5" を追記するとスコア降順になり、かつ上位5件のみ取得できます。
-//     ORDER BYは検索結果の並び替え、LIMITは取得件数の制限に使います。
+//スコア降順、かつ上位5件のみ取得
 $sql = "SELECT name, score FROM ranking ORDER BY score DESC LIMIT 5;";
 $r = $db->query($sql);
 if(!$r){
@@ -42,4 +35,3 @@ $result['error'] = 0;
 
 //$resultをJSON形式にして出力
 print json_encode($result);
-
