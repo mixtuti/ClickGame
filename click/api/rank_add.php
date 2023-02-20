a2f7383cc476935b1aa846cc64caab8f4dfb8d78
mixtuti桜餅 <88471532+mixtuti@users.noreply.github.com>
2023-02-20 14:52:25 +0900

    Update rank_add.php

--- click/api/rank_add.php
+++ click/api/rank_add.php
@@ -1,27 +1,19 @@
 <?php
 /**
  * ランキング登録API
- * 
- * Copyright (c) 2018 Kazuma Kasagi
- * Released under the MIT license
- * http://opensource.org/licenses/mit-license.php
  */
 
 //$resultは戻り値 error=1なら失敗、0なら成功
 $result = array('error' => 1);
 
- //登録名とスコアの引数(GET値)確認
+//登録名とスコアの引数(GET値)確認
 $name = filter_input(INPUT_GET, 'name');
 $score = filter_input(INPUT_GET, 'score');
 if(!$name || !is_numeric($score)) exit;
 
 include 'connect.php';
 
-//TODO:以下の$sql変数に、INSERT文のSQLを記述せよ。
-//     rankingテーブルに対し登録名、スコアを新規登録します。
-//     name には $name を、score には $score が入るようにしてください。
-//     phpで変数を文字列と結合するには . 記号を使います。
-//     (例) $sql = "名前は" . $name . "です。";
+//rankingテーブルに対し登録名、スコアを新規登録
 $sql = "INSERT INTO ranking(name, score) VALUES ('$name', '$score');";
 $r = $db->query($sql);
 if($r !== true){
@@ -33,4 +25,3 @@ $result['error'] = 0;
 
 //$resultをJSON形式にして出力
 print json_encode($result);
-
